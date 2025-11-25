6f11528c9c8c9d22c3a70288947a96fbb99b51c1
Andreas Harth <andreas@harth.org>
2025-11-25 22:31:23 +0100

    src reorg for tests

---
+++ src/test/java/com/ontologycentral/osmwrap/geometry/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.ontologycentral.osmwrap.geometry;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void helloWorld() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testOne() {
+        int result = 1 + 1;
+        assertTrue(result == 2);
+    }
+}
